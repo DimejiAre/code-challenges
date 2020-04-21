8ba85e4e1c1831fb0954f114b8df8b86347640af
DimejiAre <olardi2@gmail.com>
2020-04-21 16:25:26 +0100

    add solution for find rotation point

---
+++ python/find_rotation_point.py
@@ -0,0 +1,86 @@
+# Sorting, Searching
+
+"""
+Now I have a list of words that are mostly alphabetical, except they start somewhere in the middle of the alphabet, reach the end, and then start from the beginning of the alphabet. In other words, this is an alphabetically ordered list that has been "rotated." For example:
+
+  words = [
+    'ptolemaic',
+    'retrograde',
+    'supplant',
+    'undulate',
+    'xenoepist',
+    'asymptote',  # <-- rotates here!
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage',
+]
+
+Write a function for finding the index of the "rotation point," which is where I started working from the beginning of the dictionary. This list is huge (there are lots of words I don't know) so we want to be efficient here.
+"""
+
+def find_rotation_point(words):
+
+    # Find the rotation point in the list
+    
+    first = -1
+    last = len(words)
+    
+    while first + 1 < last:
+        middle = (first + last) // 2
+        
+        
+        if words[middle] < words[0]:
+            last = middle
+        else:
+            first = middle
+
+        if first + 1 == last:
+            return last
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_small_list(self):
+        actual = find_rotation_point(['cape', 'cake'])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_rotation_point(['grape', 'orange', 'plum',
+                                      'radish', 'apple'])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_large_list(self):
+        actual = find_rotation_point(['ptolemaic', 'retrograde', 'supplant',
+                                      'undulate', 'xenoepist', 'asymptote',
+                                      'babka', 'banoffee', 'engender',
+                                      'karpatka', 'othellolagkage'])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    # Are we missing any edge cases?
+
+
+unittest.main(verbosity=2)
+
+"""
